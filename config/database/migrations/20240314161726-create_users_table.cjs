cc7102a34e83ae7ac2b432d3913c5064295a7ed3
Antonio José Herrera Tabaco <jota@MacBook-Air-de-Jota.local>
2024-03-14 18:20:44 +0100

    feat: create first migration

---
+++ config/database/migrations/20240314161726-create_users_table.cjs
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING(64),
+        allowNull: true
+      },
+      provider: {
+        type: Sequelize.STRING(32),
+        defaultValue: true
+      },
+      profileImage: {
+        type: Sequelize.STRING(64),
+        allowNull: false
+      },
+      lastLogin: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
